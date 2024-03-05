4e1202a8dd637411d719bcd186e8a66bad4901de
Quentout187 <quentin.caron@ecoles-epsi.net>
2024-03-05 10:33:58 +0100

    Ajout d'un bouton pour afficher la description

--- migrations/Version20240304154605.php
+++ migrations/Version20240304154605.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240226082951 extends AbstractMigration
+final class Version20240304154605 extends AbstractMigration
 {
     public function getDescription(): string
     {
